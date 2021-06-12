2a52c1e4e15109f0d671b8c6ad907283c77b5d7d
phantomcorn <jj.phantakorn@outlook.com>
2021-06-12 10:28:15 +0700

    [UNTESTED] database implementation

--- lib/Database.dart
+++ lib/Database.dart
@@ -3,39 +3,131 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class Spent {
-  final int day;
-  final int month;
-  final int year;
+  final DateTime date;
   final double amount;
 
   Spent({
-    required this.day,
-    required this.month,
-    required this.year,
+    required this.date,
     required this.amount
   });
 
   Map<String, dynamic> toMap() {
     return {
-      'date': month.toString() + "-" + day.toString() + "-" + year.toString(),
+      'date': date.toIso8601String(),
       'amountSpent': amount
     };
   }
 
+  static Spent fromMap(Map<String, Object?> map) {
+    return Spent(
+        date : map['date'] as DateTime,
+        amount : map['amountSpent'] as double);
+  }
+
+  Spent copy({
+    DateTime? date,
+    double? amount
+  }) =>
+      Spent(
+          date: date ?? this.date,
+          amount: amount ?? this.amount
+      );
+
+
   @override
   String toString() {
-    return "spent{date: $month-$day-$year, amountSpent: $amount}";
+    return "spent{date: $date, amountSpent: $amount}";
   }
 }
 
-void main() async {
-  final database = openDatabase(
-    join(await getDatabasesPath(), "spent.db"),
-    onCreate : (db, version)  {
-      return db.execute(
-          'CREATE TABLE spent(date DATE PRIMARY KEY, amountSpent REAL)'
-      );
-    },
-    version : 1
-  );
+
+
+class spentDatabase {
+
+  static final spentDatabase instance = spentDatabase._init();
+  static Database? _database;
+  spentDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database!;
+    }
+
+    _database = await _initDB('spent.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filename) async {
+    final dbPath = await getDatabasesPath();
+    var path = join(dbPath, filename);
+
+    return await openDatabase(path, version : 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('CREATE TABLE spent(date DATE PRIMARY KEY, amountSpent REAL)');
+  }
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
+
+
+  Future<void> insertAmount(Spent data) async {
+    final db = await instance.database;
+
+    final id = await db.insert(
+        'spent',
+        data.toMap(),
+        conflictAlgorithm : ConflictAlgorithm.replace
+    );
+
+  }
+  
+  Future<Spent> readAmount(DateTime date) async {
+    final db = await instance.database;
+    final map = await db.query(
+        'spent',
+        columns: ['date','amountSpent'],
+        where: 'date = ?',
+        whereArgs: [date.toIso8601String()]);
+
+    if (map.isNotEmpty) {
+      return Spent.fromMap(map.first);
+    } else {
+      throw Exception('No query exists from database');
+    }
+  }
+
+  Future<int> accumulateAmount(Spent spent) async {
+    final db = await instance.database;
+    var prevRead = await readAmount(spent.date);
+    var prevAmt = prevRead.amount;
+    var newAmt = prevAmt + spent.amount;
+    Spent newSpent = Spent(date : spent.date, amount : newAmt);
+    return db.update(
+        'spent',
+        newSpent.toMap(),
+        where: 'date = ?',
+        whereArgs: [spent.date.toIso8601String()]
+    );
+  }
+
+  Future<List<Spent>> getAll() async {
+    final db = await instance.database;
+    final result = await db.query('spent');
+
+    return result.map((map) => Spent.fromMap(map)).toList();
+  }
+
+  Future<double> getTotalSpending() async {
+    List<Spent> results = await getAll();
+
+    if (results.isEmpty) {
+      return 0;
+    }
+
+    return results.map((spent) => spent.amount).reduce((value, element) => value + element);
+  }
 }

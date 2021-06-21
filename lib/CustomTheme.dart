016ce7a953ff0271a71f66e8d28baaab6f810db6
phantomcorn <jj.phantakorn@outlook.com>
2021-06-21 10:49:56 +0700

    adjust fonts of button and date picker

--- lib/CustomTheme.dart
+++ lib/CustomTheme.dart
@@ -16,6 +16,17 @@ class customTheme {
               borderRadius: BorderRadius.circular(10)
           ),
           primary : const Color.fromRGBO(169, 169, 169, 1),
+          textStyle: TextStyle(
+            fontSize: 40
+          )
+        )
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          primary: const Color.fromRGBO(169, 169, 169, 1),
+          textStyle: TextStyle(
+            fontSize: 30
+          )
         )
       )
     );

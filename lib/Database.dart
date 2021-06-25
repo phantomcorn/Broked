d9dc536136d8116abae91221634e694d5a405d33
phantomcorn <jj.phantakorn@outlook.com>
2021-06-25 23:03:40 +0700

    add optional budget field to db + add opt property to class Spent

--- lib/Database.dart
+++ lib/Database.dart
@@ -6,10 +6,12 @@ import 'package:intl/intl.dart';
 class Spent {
   final DateTime date;
   final double amount;
+  final double? budget;
 
   Spent({
     required this.date,
-    required this.amount
+    required this.amount,
+    this.budget
   });
 
   static String dateToSQLFormat(DateTime date) {
@@ -20,14 +22,17 @@ class Spent {
   Map<String, dynamic> toMap() {
     return {
       'date': dateToSQLFormat(date),
-      'amountSpent': amount
+      'amountSpent': amount,
+      'budget' : budget?? null
     };
   }
 
   static Spent fromMap(Map<String, Object?> map) {
     return Spent(
         date : DateTime.parse(map['date'] as String),
-        amount : map['amountSpent'] as double);
+        amount : map['amountSpent'] as double,
+        budget : map['budget'] as double? ?? null
+    );
   }
 
   Spent copy({
@@ -42,7 +47,7 @@ class Spent {
 
   @override
   String toString() {
-    return "spent{date: $date, amountSpent: $amount}";
+    return "spent{date: $date, amountSpent: $amount, budget: ${budget?? null}";
   }
 }
 
@@ -70,12 +75,22 @@ class spentDatabase {
   Future<Database> _initDB(String filename) async {
     final dbPath = await getDatabasesPath();
     var path = join(dbPath, filename);
+    print(path);
 
     return await openDatabase(path, version : 1, onCreate: _createDB);
   }
 
   Future _createDB(Database db, int version) async {
-    await db.execute('CREATE TABLE spent(date DATE PRIMARY KEY, amountSpent REAL)');
+    await db.execute('CREATE TABLE spent('
+        'date DATE PRIMARY KEY NOT NULL,'
+        'amountSpent REAL NOT NULL,'
+        'budget REAL'
+        ')'
+    );
+  }
+
+  DateTime startOfMonth(DateTime date) {
+    return DateTime(date.year, date.month, 1);
   }
   
   Future<void> deleteAllRecords() async {
@@ -108,7 +123,11 @@ class spentDatabase {
     final db = await instance.database;
     var prevAmt = await getSpendingByDate(oldSpent.date);
     var newAmt = prevAmt + oldSpent.amount;
-    Spent newSpent = Spent(date : oldSpent.date, amount : newAmt);
+    Spent newSpent = Spent(
+        date : oldSpent.date,
+        amount : newAmt,
+        budget: oldSpent.budget ?? null
+    );
     return db.insert(
         'spent',
         newSpent.toMap(),
@@ -170,6 +189,28 @@ class spentDatabase {
         .toList()
         .reduce((value, element) => value + element);
   }
+
+  Future addBudget(double budget) async {
+    final db = await instance.database;
+    DateTime now = DateTime.now();
+    if (await getSpendingByDate(startOfMonth(now)) == 0) {
+      print("creating new");
+      Spent spent = Spent(
+        date: startOfMonth(now),
+        amount: 0,
+        budget: budget
+      );
+      await accumulateAmount(spent);
+    } else {
+      await db.rawUpdate(
+          """UPDATE spent
+             SET budget = ?
+             WHERE date = ? """,
+          [DateTime(now.year, now.month, 1)]
+      );
+    }
+  }
+
   
 
   Future<double> getSpendingToday() async {

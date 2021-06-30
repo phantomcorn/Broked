582d7d00107923e7005679e1c1c2d3cf29873e9c
phantomcorn <jj.phantakorn@outlook.com>
2021-06-30 12:29:04 +0700

    change "Theme" to "theme"

--- lib/Theme.dart
+++ lib/Theme.dart
@@ -7,17 +7,17 @@ class customTheme {
 
   static ThemeData get defaultTheme {
     return ThemeData(
-      scaffoldBackgroundColor: MyApp.Theme[MyApp.selectedTheme]!["bg"],
+      scaffoldBackgroundColor: MyApp.theme[MyApp.selectedTheme]!["bg"],
       textTheme: TextTheme(
         bodyText1: GoogleFonts.nunitoTextTheme().bodyText1,
         bodyText2: GoogleFonts.nunitoTextTheme().bodyText2
       ).apply(
-        bodyColor: MyApp.Theme[MyApp.selectedTheme]!["text"],
-        displayColor: MyApp.Theme[MyApp.selectedTheme]!["text"]
+        bodyColor: MyApp.theme[MyApp.selectedTheme]!["text"],
+        displayColor: MyApp.theme[MyApp.selectedTheme]!["text"]
       ),
       textButtonTheme: TextButtonThemeData(
         style: TextButton.styleFrom(
-          primary: MyApp.Theme[MyApp.selectedTheme]!["dateButton"],
+          primary: MyApp.theme[MyApp.selectedTheme]!["dateButton"],
           textStyle: TextStyle(
             fontSize: 30,
             fontFamily: GoogleFonts.nunito().fontFamily
